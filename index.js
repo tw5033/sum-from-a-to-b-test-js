f9b9f117b4e3598287a3963b332aa40da3bdb046
tw5033 <tw5033@gmail.com>
2019-05-02 07:10:15 +0000

    Finish recursive function

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 
 function sum(fromN, toN) {
-  // Sum all the values from fromN up to toN
+  // base case when fromN = toN 
+  if(toN === fromN) {
+    return fromN;
+  }
+  return fromN + sum(fromN + 1, toN);
 }
 
 module.exports = sum;
